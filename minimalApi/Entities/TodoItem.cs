10f175fca78e65e299df2de85bfe49ad230a3dec
Chetan Soni <chetansoni8101@gmail.com>
2025-01-19 12:30:50 +0530

    Created minimal Api for the backend

---
+++ minimalApi/Entities/TodoItem.cs
@@ -0,0 +1,5 @@
+public class TodoItem {
+    public int Id {get;set;}
+    public string Task {get;set;} = string.Empty;
+    public bool isCompleted {get;set;}
+}
--- minimalApi/Program.cs
+++ minimalApi/Program.cs
@@ -1,6 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/todo", async (AppDbContext db) => {
+    return await db.Todos.ToListAsync();
+});
+
+app.MapGet("/todo/{id}", async (int id, AppDbContext db) => {
+    var todo = await db.Todos.FindAsync(id);
+
+    return todo is not null ? Results.Ok(todo) : Results.NotFound();
+});
+
+app.MapPost("/todo", async (TodoItem todoItem, AppDbContext db) => {
+    db.Todos.Add(todoItem);
+
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todo/{todoItem.Id}", todoItem);
+});
+
+app.MapPut("/todo/{id}", async (int id, TodoItem todoItem, AppDbContext db) => {
+    var item = await db.Todos.FindAsync(id);
+
+    if(item is null) {
+        return Results.NotFound();
+    }
+
+    item.Task = todoItem.Task;
+    item.isCompleted = todoItem.isCompleted;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/todo/{id}", async ( int id, AppDbContext db) => {
+    var item = await db.Todos.FindAsync(id);
+
+    if (item is null) {
+        return Results.NotFound();
+    }
+
+    db.Todos.Remove(item);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
 
 app.Run();
